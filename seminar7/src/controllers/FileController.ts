be6054c67b725c672da6b99ef01e3c10b91a233c
coreminw <coreminw@naver.com>
2022-06-06 02:14:09 +0900

    feat: FileController 기능 추가

--- seminar7/src/controllers/FileController.ts
+++ seminar7/src/controllers/FileController.ts
@@ -26,6 +26,23 @@ const uploadFileToS3 = async (req: Request, res: Response) => {
 
 const uploadFilesToS3 = async (req: Request, res: Response) => {
     if(!req.files) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, message.BAD_REQUEST));
+
+    const images: Express.MulterS3.File[] = req.files as Express.MulterS3.File[];
+
+    try {
+        const imageList: {
+            location: string,
+            originalname: string
+        }[] = await Promise.all(images.map((image: Express.MulterS3.File) =>{
+            return {
+                location: image.location,
+                originalname: image.originalname
+            };
+        }));
+    } catch (error) {
+        console.log(error);
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+    }
 }
 
 export default {
